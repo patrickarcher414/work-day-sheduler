9778143643ce5eb58aea82466ac40e2d00ccd08a
patrickarcher414 <patrickarcher414@gmail.com>
2022-04-03 16:52:43 -0500

    color coding, and text input

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,53 @@
 
 var currentDay = document.querySelector('#currentDay')
 var timeBlocks = document.querySelector('.container')
-var color 
-var d = new Date();
-var time
-var currentHour = d.getHours();
+var currentHour = moment().format('H');
 var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
-
-// color-code time blocks. past = grey, present = red, future = green.
-// function colorCode() {
-//     if (currentHour > 12) {
-//         currentHour - 12;
-//     }
-//     if (getTime < currentHour) {
-//         color = '#DBD4D3';
-//     } 
-//     if (getTime === currentHour) {
-//         color = '#E83151';
-//     }
-//     if (getTime > currentHour) {
-//         color = '#D2CCA1';
-//     }
-// } 
-
-// get time value from time-blocks
-var time = function() {
-    for (var i=0; i < hours.length; i++) {
-        parseInt(hours[i]) 
-    }
-}
-console.log(time)
+var eventInput = []
 
 // create time-blocks
 function createTimeBlocks() {
     for (var i=0; i < hours.length; i++) {
+        var row = document.createElement('div')
+        row.classList.add('row')
         var hour = document.createElement('h3')
-        var block = document.createElement('p')
-        block.style.padding = '30px';
-        block.style.border = 'solid black 1px';
-        // if (getTime)
-        block.style.backgroundColor = '#DBD4D3';
+        hour.classList.add('hour')
         hour.innerText = hours[i]
-        timeBlocks.appendChild(hour)
-        timeBlocks.appendChild(block)
+        var eventText = document.createElement('textarea')
+        eventText.setAttribute('id', i)
+      
+        // color code time blocks past, present, future
+        var time = i + 9;
+        if (currentHour > time) {
+            eventText.classList.add('col-lg-7', 'past')
+        } else if (currentHour < time){
+            eventText.classList.add('col-lg-7', 'future')
+        } else {
+            eventText.classList.add('col-lg-7', 'present')
+        }
+
+        var button = document.createElement('button')
+        button.classList.add('saveBtn', 'col-lg-2');
+        button.addEventListener('click', function(ev) {
+            ev.preventDefault();
+           // I can not figure out how to store textarea input...
+        })
+
+        row.appendChild(hour)
+        row.appendChild(eventText)
+        row.appendChild(button)
+        timeBlocks.appendChild(row)
     }
 }
 
-// display current day in currentDay p
-function dateDisplay() {
-    var month = d.getMonth() + 1;
-    var day = d.getDate();
-    var year = d.getFullYear();
-    var today = currentDay.innerHTML = month + "/" + day + "/" + year
-};
 
-dateDisplay();
+
+// display current day in #currentDay
+
+currentDay.innerText = moment().format('MMMM Do YYYY');
+
+
 createTimeBlocks();
-// colorCode();
-getTime();
+
+
 
